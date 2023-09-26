fc1b45ba5d5a0c72d7291f389bc14fc7b509ad23
Cameron Lowell Palmer <cameron@promon.no>
2023-09-26 11:27:43 +0200

    Add the data file

---
+++ app/src/main/java/no/kristiania/carouselhorse/CarouselHorses.kt
@@ -0,0 +1,22 @@
+package no.kristiania.carouselhorse
+
+enum class Directions {
+    Unknown,
+    Forward,
+    Backward
+}
+
+val carouselOfHorses = listOf(
+    R.drawable.horse01,
+    R.drawable.horse02,
+    R.drawable.horse03,
+    R.drawable.horse04,
+    R.drawable.horse05,
+    R.drawable.horse06,
+    R.drawable.horse07,
+    R.drawable.horse08,
+    R.drawable.horse09,
+    R.drawable.horse10,
+    R.drawable.horse11)
+
+val standingHorse = R.drawable.horse12
